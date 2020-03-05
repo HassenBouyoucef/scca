21b1e408ea38f39265146a5c98c157174ca6f589
Traore <moussa.traoreinfo@gmail.com>
2020-03-05 10:10:19 +0100

    Ui application

--- src/main/java/ui/DlgApp.java
+++ src/main/java/ui/DlgApp.java
@@ -1,13 +1,67 @@
 package ui;
 
+import javax.swing.*;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
-public class DlgApp extends Dialog {
+public class DlgApp extends JDialog {
+    DlgApp oJFrame = this;
+    JList jList = new JList<>();
+    JButton jButton = new JButton("Build another Application");
 
+    JLabel jLabel = new JLabel("Did you appreciate this application ?");
+    private String[] notelist = {"1", "2", "3", "4", "5"};
 
-    public DlgApp(Frame owner, String title, boolean modal) {
+    public DlgApp(JFrame owner, String title, boolean modal) {
         super(owner, title, modal);
+
+        this.setSize(550, 350);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        this.initComponent();
+
+
     }
 
+    private void initComponent() {
+        JPanel content = new JPanel();
+        content.setBackground(Color.white);
+        content.add(panelCenter());
+        content.add(jButton);
+        add(content);
+        listenerEvent();
+    }
+
+
+    private JPanel panelCenter() {
+        JPanel jPanel = new JPanel();
+        jPanel.setBackground(Color.white);
+        jPanel.setPreferredSize(new Dimension(500, 250));
+        jPanel.setBorder(BorderFactory.createTitledBorder("Application construit"));
+        jPanel.add(jList);
+
+        return jPanel;
+    }
+
+    private void listenerEvent() {
+        jButton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int selection = JOptionPane.showOptionDialog(null, jLabel, "Note pour application",
+                        JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, notelist,
+                        notelist[0]);
+
+                if (selection > 0) {
+                    System.out.println("your appreciation is: " + notelist[selection]);
+                }
+                dispose();
+            }
+
+        });
+
+
+    }
 
 }
--- src/main/java/ui/IHMUses.java
+++ src/main/java/ui/IHMUses.java
@@ -11,13 +11,12 @@ import java.awt.event.ActionEvent;
 
 
 public class IHMUses extends JFrame {
-private Class<IHMUses> logger=IHMUses.class;
+    private Class<IHMUses> logger = IHMUses.class;
     private final String nameApp = "SCAA";
-
+    private IHMUses ihmUses;
     private JButton valideButton = new JButton("Build your application");
     private JLabel titleLaabel = new JLabel("Please select a Component");
     private JComboBox<Composant> composantJComboBox = new JComboBox<>();
-
 
 
     public IHMUses() {
@@ -29,18 +28,19 @@ private Class<IHMUses> logger=IHMUses.class;
         initialisation();
         listenerEvent();
     }
-private void initialisation(){
-    Scan scan=new Scan();
-    for(Composant cm:scan.scanEnvironement()){
-        composantJComboBox.addItem(cm);
-    }
 
-}
+    private void initialisation() {
+        Scan scan = new Scan();
+        for (Composant cm : scan.scanEnvironement()) {
+            composantJComboBox.addItem(cm);
+        }
+
+    }
 
     private JPanel principalPanel() {
         JPanel mainPanel = new JPanel();
         Border compound = null;
-        composantJComboBox.setPreferredSize(new Dimension(120,20));
+        composantJComboBox.setPreferredSize(new Dimension(120, 20));
         compound = BorderFactory.createTitledBorder(compound, "SCAA Construction", TitledBorder.LEFT, TitledBorder.ABOVE_TOP);
         mainPanel.setBorder(compound);
 
@@ -52,11 +52,17 @@ private void initialisation(){
     }
 
 
-    private void listenerEvent(){
+    private void listenerEvent() {
+        ihmUses = this;
         valideButton.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-JOptionPane.showMessageDialog(null,composantJComboBox.getSelectedItem());
+                JOptionPane.showMessageDialog(null, composantJComboBox.getSelectedItem());
+
+                DlgApp dlgApp = new DlgApp(ihmUses, "Application", true);
+
+                dlgApp.setVisible(true);
+
             }
         });
     }
